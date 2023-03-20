35761d0c6085c4350f3b0ef12531f90ef2977751
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-20 23:58:30 +0400

    Task is done

---
+++ 01_prime/02_arrays/038_arrays-intro.js
@@ -0,0 +1,11 @@
+// 1. Создайте массив с числами. Выведите его на экран с помощью функции alert, а также выведите его в консоль с помощью console.log.
+
+let arrayOfNumbers = [1, 2, 3, 4];
+alert(arrayOfNumbers);
+console.log(arrayOfNumbers);
+
+// 2. Создайте массив со строками. Выведите его на экран с помощью функции alert, а также выведите его в консоль с помощью console.log.
+
+let arrayOfStrings = ['one', 'two', 'three'];
+alert(arrayOfStrings);
+console.log(arrayOfStrings);
