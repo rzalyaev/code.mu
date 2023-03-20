1d719bcf7415e852933228150196f2fbced74334
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-21 02:18:10 +0400

    Initial commit, add task solution

---
+++ 01_prime/03_objects/050_objects-string-keys.js
@@ -0,0 +1,4 @@
+// 1. Создайте объект user с ключами 'name', 'surname', 'patronymic' и какими-то произвольными значениями. Выведите на экран фамилию, имя и отчество через пробел.
+
+let user = {name: 'Ruslan', surname: 'Zalyaev', patronymic: 'Firdausovich'};
+console.log(`${user['name']} ${user['surname']} ${user['patronymic']}`);
