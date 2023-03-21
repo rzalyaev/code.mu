f201b804c5ecdbc76dbdc21502caba08d219fa20
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-21 23:56:52 +0400

    Initial commit. Add task solution

---
+++ 01_prime/03_objects/060_objects-property-var-mistake.js
@@ -0,0 +1,13 @@
+// 1. Исправьте ошибку, допущенную в следующем коде:
+
+let obj = {x: 1, y: 2, z: 3};
+
+let prop = 'x';
+console.log(obj[prop]); // Исправлено. Было: console.log(obj.prop);
+
+// 2. Исправьте ошибку, допущенную в следующем коде:
+
+obj = {x: 1, y: 2, z: 3};
+
+prop = 'x';
+console.log(obj[prop]); // Исправлено. Было: console.log(obj['prop']);
