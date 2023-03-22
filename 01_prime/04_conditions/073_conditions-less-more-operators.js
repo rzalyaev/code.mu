a117be0decbb96c34f7f9a66fc948c284ba1d682
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-22 23:14:15 +0400

    Initial commit, add task solution

---
+++ 01_prime/04_conditions/073_conditions-less-more-operators.js
@@ -0,0 +1,33 @@
+let test = 1;
+
+// 1. Проверьте, что переменная test больше 10.
+
+if (test > 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// 2. Проверьте, что переменная test меньше 10.
+
+if (test < 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// 3. Проверьте, что переменная test больше или равна 10.
+
+if (test >= 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+// 4. Проверьте, что переменная test меньше или равна 10.
+
+if (test <= 10) {
+    console.log(true);
+} else {
+    console.log(false);
+}
