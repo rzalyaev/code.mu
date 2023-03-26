ae0f267ad18240f36ed6eade7335d9a2ec3fc583
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-26 23:53:26 +0400

    Initial commit, Add task solution

---
+++ 01_prime/04_conditions/077_conditions-strings-equality.js
@@ -0,0 +1,11 @@
+let test1 = 'abc';
+let test2 = 'abc';
+
+// 1. Даны переменные test1 и test2, содержащие строки.
+//    Проверьте, равны ли их значения и выведите соответствующее сообщение.
+
+if (test1 == test2) {
+    console.log('test1 и test2 равны');
+} else {
+    console.log('test1 и test2 неравны');
+}
