7487ecb2a103c0bc0a2a04806178e2189d3d6da1
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-26 23:53:22 +0400

    Initial commit, Add task solution

---
+++ 01_prime/04_conditions/076_conditions-variables-comparison.js
@@ -0,0 +1,18 @@
+let test1 = 1;
+let test2 = 2;
+
+// 1. Даны переменные test1 и test2. Проверьте, значение какой из этих переменных больше и выведите соответствующее сообщение.
+
+if (test1 > test2) {
+    console.log(`test1 больше test2`);
+} else {
+    console.log(`test2 больше test1`);
+}
+
+// 2. Даны переменные test1 и test2. Проверьте, равны ли их значения и выведите соответствующее сообщение.
+
+if (test1 == test2) {
+    console.log(`test1 и test2 равны`);
+} else {
+    console.log(`test1 и test2 неравны`)
+}
