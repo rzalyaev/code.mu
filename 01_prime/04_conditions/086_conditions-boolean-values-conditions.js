f6be97c57132196f51a51855971d9284b4170a25
rzalyaev <ruslan.zalyaev@gmail.com>
2023-03-28 23:56:20 +0400

    Add task solution

---
+++ 01_prime/04_conditions/086_conditions-boolean-values-conditions.js
@@ -0,0 +1,15 @@
+// 1. Проверьте, что переменная test равна true.
+
+    if (test === true) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+// 2. Проверьте, что переменная test равна false.
+
+    if (test === false) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
