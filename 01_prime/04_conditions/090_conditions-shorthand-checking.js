5da8b13148169ee280dec2cf8edd048e060293c1
rzalyaev <ruslan.zalyaev@gmail.com>
2023-04-06 07:13:08 +0400

    Add tasks solutions

---
+++ 01_prime/04_conditions/090_conditions-shorthand-checking.js
@@ -0,0 +1,119 @@
+// 1. Не запуская код, определите, что выведется в консоль:
+
+    let test = 3;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '+++'.
+
+// 2. Не запуская код, определите, что выведется в консоль:
+
+    let test = 'abc';
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '+++'.
+
+// 3. Не запуская код, определите, что выведется в консоль:
+
+    let test = '';
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 4. Не запуская код, определите, что выведется в консоль:
+
+    let test = 3 * 'abc';
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 5. Не запуская код, определите, что выведется в консоль:
+
+    let test = null;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 6. Не запуская код, определите, что выведется в консоль:
+
+    let test = false;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 7. Не запуская код, определите, что выведется в консоль:
+
+    let test;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 8. Не запуская код, определите, что выведется в консоль:
+
+    let test = 0;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
+
+// 9. Не запуская код, определите, что выведется в консоль:
+
+    let test = '0';
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '+++'.
+
+// 10. Не запуская код, определите, что выведется в консоль:
+
+    let test = -1;
+
+    if (test) {
+        console.log('+++');
+    } else {
+        console.log('---');
+    }
+
+    // Answer: '---'.
